61a34c2fc8a3311e2bfe69ef8eea0bbaec202d67
vypiemzalyubov <dima.rza90@mail.ru>
2023-09-08 21:47:00 +0300

    Add test to auth

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -28,8 +28,9 @@ class TestUserAuth(BaseCase):
         self.token = self.get_header(response1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
-
+    
     @allure.description("Авторизовываем пользователя и проверяем, что пользователь авторизовался успешно")
+    @pytest.mark.positive
     def test_auth_user(self):
 
         response2 = MyRequests.get(
@@ -43,4 +44,28 @@ class TestUserAuth(BaseCase):
             "user_id",
             self.user_id_from_auth_method,
             "User id from auth method is not equal to user id from check method"
+        )
+
+
+    @allure.description("Авторизация пользователя без одного обязательного параметра - хедера или токена")
+    @pytest.mark.negative
+    @pytest.mark.parametrize("condition", exclude_params)
+    def test_negative_auth_check(self, condition):
+        
+        if condition == "no_cookie":
+            response2 = MyRequests.get(
+                "/user/auth",
+                headers={"x-csrf-token": self.token}
+            )
+        else:
+            response2 = MyRequests.get(
+                "/user/auth",
+                cookies={"auth_sid": self.auth_sid}
+            )
+
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            0,
+            f"User is authorized with condition {condition}"
         )
