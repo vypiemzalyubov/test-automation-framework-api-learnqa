8ac90c16e3fccdb3936803bf9d2afe3f791610c3
vypiemzalyubov <dima.rza90@mail.ru>
2023-09-09 10:58:21 +0300

    Add test_delete_admin_user

--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -6,5 +6,30 @@ from lib.my_requests import MyRequests
 
 
 @allure.epic("User deleting cases")
-class TestUserEdit(BaseCase):
-    pass
+class TestUserDelete(BaseCase):
+    
+
+    @allure.description("Авторизовываем пользователя и проверяем, что пользователя с правами администратора нельзя удалить")
+    @pytest.mark.negative
+    def test_delete_admin_user(self):
+
+        # LOGIN
+        data = {
+            "email": "vinkotov@example.com",
+            "password": "1234"
+        }
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+        # DELETE
+        response2 = MyRequests.delete(
+            f"/user/{user_id_from_auth_method}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )        
+        
+        Assertions.assert_code_status(response2, 400)
+        Assertions.assert_response_text_value(response2, "Please, do not delete test users with ID 1, 2, 3, 4 or 5.")
